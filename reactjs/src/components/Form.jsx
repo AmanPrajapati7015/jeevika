60916a566d9cd19dd340fa84169f264e41936705
bqka <aditya2005mendiratta@gmail.com>
2024-04-13 05:12:39 +0530

    changes

--- reactjs/src/components/Form.jsx
+++ reactjs/src/components/Form.jsx
@@ -6,6 +6,7 @@ import SectionD from "../form-elements/SectionD";
 import SectionE from "../form-elements/SectionE";
 import SectionF from "../form-elements/SectionF";
 import Decleration from "../form-elements/Declaration";
+import {bgimage} from '../assets/bgimage1.jpg'
 import "./form.css";
 
 export default function Form() {
@@ -28,91 +29,106 @@ export default function Form() {
   }
 
   return (
-    <div className="form-container">
-      <div className="form-section-container">
-        <div
-          className="form-section"
-          onClick={(e) => {
-            setIsActive("A");
-          }}
-        >
-          Section A
+    <>
+    <img className="wrap" src={bgimage} />
+      <div className="form-container">
+        <div className="form-section-container">
+          <div
+            className="form-section"
+            onClick={(e) => {
+              setIsActive("A");
+            }}
+          >
+            Section A
+          </div>
+          <div
+            className="form-section"
+            onClick={(e) => {
+              setIsActive("B");
+            }}
+          >
+            Section B
+          </div>
+          <div
+            className="form-section"
+            onClick={(e) => {
+              setIsActive("C");
+            }}
+          >
+            Section C
+          </div>
+          <div
+            className="form-section"
+            onClick={(e) => {
+              setIsActive("D");
+            }}
+          >
+            Section D
+          </div>
+          <div
+            className="form-section"
+            onClick={(e) => {
+              setIsActive("E");
+            }}
+          >
+            Section E
+          </div>
+          <div
+            className="form-section"
+            onClick={(e) => {
+              setIsActive("F");
+            }}
+          >
+            Section F
+          </div>
+          <div
+            className="form-section"
+            onClick={(e) => {
+              setIsActive("Decleration");
+            }}
+          >
+            Decleration
+          </div>
         </div>
-        <div
-          className="form-section"
-          onClick={(e) => {
-            setIsActive("B");
-          }}
-        >
-          Section B
-        </div>
-        <div
-          className="form-section"
-          onClick={(e) => {
-            setIsActive("C");
-          }}
-        >
-          Section C
-        </div>
-        <div
-          className="form-section"
-          onClick={(e) => {
-            setIsActive("D");
-          }}
-        >
-          Section D
-        </div>
-        <div
-          className="form-section"
-          onClick={(e) => {
-            setIsActive("E");
-          }}
-        >
-          Section E
-        </div>
-        <div
-          className="form-section"
-          onClick={(e) => {
-            setIsActive("F");
-          }}
-        >
-          Section F
-        </div>
-        <div
-          className="form-section"
-          onClick={(e) => {
-            setIsActive("Decleration");
-          }}
-        >
-          Decleration
+        <div className="form-area">
+          {isActive === "A" ? (
+            <SectionA state={state} handleChange={handleChange} />
+          ) : (
+            ""
+          )}
+          {isActive === "B" ? (
+            <SectionB state={state} handleChange={handleChange} />
+          ) : (
+            ""
+          )}
+          {isActive === "C" ? (
+            <SectionC state={state} handleChange={handleChange} />
+          ) : (
+            ""
+          )}
+          {isActive === "D" ? (
+            <SectionD state={state} handleChange={handleChange} />
+          ) : (
+            ""
+          )}
+          {isActive === "E" ? (
+            <SectionE state={state} handleChange={handleChange} />
+          ) : (
+            ""
+          )}
+          {isActive === "F" ? (
+            <SectionF state={state} handleChange={handleChange} />
+          ) : (
+            ""
+          )}
+          {isActive === "Decleration" ? (
+            <Decleration state={state} handleChange={handleChange} />
+          ) : (
+            ""
+          )}
+          {/* {console.log(state)} */}
         </div>
       </div>
-      <div className="form-area">
-        {isActive === "A" ? (
-          <SectionA state={state} handleChange={handleChange} />
-        ) : (
-          ""
-        )}
-        {isActive === "B" ? (
-          <SectionB state={state} handleChange={handleChange}/>
-        ) : (
-          ""
-        )}
-        {isActive === "C" ? (
-          <SectionC state={state} handleChange={handleChange} />
-        ) : (
-          ""
-        )}
-        {isActive === "D" ? <SectionD state={state} handleChange={handleChange} /> : ""}
-        {isActive === "E" ? <SectionE state={state} handleChange={handleChange} /> : ""}
-        {isActive === "F" ? <SectionF state={state} handleChange={handleChange} /> : ""}
-        {isActive === "Decleration" ? (
-          <Decleration state={state} handleChange={handleChange} />
-        ) : (
-          ""
-        )}
-        {/* {console.log(state)} */}
-      </div>
-    </div>
+    </>
   );
 }
--- reactjs/src/components/Navbar.jsx
+++ reactjs/src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import "./Navbar.css";
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo-removebg-preview.png";
 import Button from 'react-bootstrap/Button'
 
 export default function Navbar() {
   return (
     <div className="nav-container">
-      {/* <div className="logo-container"> */}
-              {/* <label htmlFor="logo-name-ji logo-name-vika"> */}
+      {/* <div className="logo-container">
+              <label htmlFor="logo-name-ji logo-name-vika"> */}
                   <img src={logo} alt="logo" className="jeevika-logo"/>
-        {/* <font id="logo-name-ji">जी</font><font id="logo-name-vika">vika</font> */}
-        {/* </label> */}
-        {/* <img className="logo-img" src="" /> */}
-      {/* </div> */}
+        {/* <font id="logo-name-ji">जी</font><font id="logo-name-vika">vika</font>
+        </label>
+        <img className="logo-img" src="" />
+      </div> */}
       <div className="nav-button-container">
         <span className="support">
           <a href="" className="supportbtn-link">
