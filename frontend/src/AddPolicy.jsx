1e75028269fc2bf0ef0ba98de483af2aae606f30
Aman Prajapati <aman@Amans-MacBook-Air.local>
2024-04-12 19:37:20 +0530

    new branch checkpoint

--- frontend/src/AddPolicy.jsx
+++ frontend/src/AddPolicy.jsx
@@ -1,9 +1,41 @@
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import { TextField, Button } from '@mui/material/';
 
 function AddPolicy(){
+    const navigate = useNavigate();
+
+    const [state, setState] = useState({});
+    
+    function setPolicy(e){
+        let curState  = {...state};
+        curState.PolicyNo = e.target.value;
+        setState(curState);
+    }
+    function setName(e){
+        let curState = {...state};
+        curState.Name = e.target.value;
+        setState(curState);
+    }
+
+    function submit(){
+        console.log(state);
+    }
+
     return(
         <>
-            <h1>Add new Policy</h1>
+            <div className="background-wrap">
+                <div className="container">
+                    <h1>Add new Details </h1>
+                    <div className="crudential" variant="outlined" >
+                        <TextField onChange={setPolicy} id="outlined-basic" label="Admin-id" variant="outlined" />
+                        <TextField onChange={setName} id="outlined-basic" type="password" label="Password" variant="outlined" />
+                        <Button onClick={submit} variant="contained">Submit</Button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
 export default AddPolicy
+
--- frontend/src/AdminLogin.jsx
+++ frontend/src/AdminLogin.jsx
@@ -1,35 +1,33 @@
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useState } from 'react';
 import { TextField, Button } from '@mui/material/';
 
 
-function AdminLogin(){
+function AdminLogin() {
 
-    const [adminID, setAdminID]  = useState("");
+    const [adminID, setAdminID] = useState("");
     const [password, setPassword] = useState("");
-    
+
     const navigate = useNavigate();
-    
-    function login(){
-        console.log({adminID, password});
+
+    function login() {
+        console.log({ adminID, password });
         navigate('/dashboard');
     }
 
-    return(
-        
+    return (
+
         <>
-        <div className="container">
-
-            <h1>Company Admin Login</h1>
-            <div className="crudential" variant="outlined" >
-
-                <div className="crudential" variant="outlined" >
-                    <TextField onChange={(e)=>setAdminID(e.target.value)} id="outlined-basic" label="Admin-id" variant="outlined" />
-                    <TextField onChange={(e)=>setPassword(e.target.value)} id="outlined-basic" type="password" label="Password" variant="outlined" />
-                    <Button onClick={login} variant="contained">Submit</Button>
+            <div className="background-wrap">
+                <div className="container">
+                    <h1>Company Admin Login</h1>
+                    <div className="crudential" variant="outlined" >
+                        <TextField onChange={(e) => setAdminID(e.target.value)} id="outlined-basic" label="Admin-id" variant="outlined" />
+                        <TextField onChange={(e) => setPassword(e.target.value)} id="outlined-basic" type="password" label="Password" variant="outlined" />
+                        <Button onClick={login} variant="contained">Submit</Button>
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
         </>
     )
 }
