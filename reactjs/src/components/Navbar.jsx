2d68b1dab3e87660214e0470ca3d2a0dac858479
bqka <aditya2005mendiratta@gmail.com>
2024-04-12 13:49:24 +0530

    Modified navbar

--- reactjs/src/components/Navbar.jsx
+++ reactjs/src/components/Navbar.jsx
@@ -1,29 +1,30 @@
-import './Navbar.css'
-import logo from '../assets/ghar.png'
+import "./Navbar.css";
+import logo from "../assets/ghar.png";
 
 export default function Navbar() {
-    return (
-      <div className="nav-container">
-        <span className="logo">
-          <img className="logo-img" src="" />
-        </span>
-        <div className="nav-button-container">
-            <span className="support">
-            <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
-              <button className="support-btn">Track Status</button>
-            </a>
-          </span>
-          <span className="signin">
-            <a href="" className="signinbtn-link">
-              <button className="support-btn">Sign In</button>
-            </a>
-          </span>
-          <span className="support">
-            <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
-              <button className="support-btn">Support</button>
-            </a>
-          </span>
-        </div>
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <font className="logo-name-ji">जी</font><font className="logo-name-vika">vika</font>
+        {/* <img className="logo-img" src="" /> */}
       </div>
-    );
+      <div className="nav-button-container">
+        <span className="support">
+          <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
+            <button className="support-btn">Track Status</button>
+          </a>
+        </span>
+        <span className="signin">
+          <a href="" className="signinbtn-link">
+            <button className="support-btn">Sign In</button>
+          </a>
+        </span>
+        <span className="support">
+          <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
+            <button className="support-btn">Support</button>
+          </a>
+        </span>
+      </div>
+    </div>
+  );
 }
