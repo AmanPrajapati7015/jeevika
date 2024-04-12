48e06f3d70b2cfa78ea6415bbb11a4ed17e2bbaf
bqka <aditya2005mendiratta@gmail.com>
2024-04-12 14:08:25 +0530

    ptani kya kr rha hu

--- reactjs/src/components/Navbar.jsx
+++ reactjs/src/components/Navbar.jsx
@@ -1,27 +1,30 @@
 import "./Navbar.css";
 import logo from "../assets/ghar.png";
+import Button from 'react-bootstrap/Button'
 
 export default function Navbar() {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <font className="logo-name-ji">जी</font><font className="logo-name-vika">vika</font>
+        <label htmlFor="logo-name-ji logo-name-vika">
+        <font id="logo-name-ji">जी</font><font id="logo-name-vika">vika</font>
+        </label>
         {/* <img className="logo-img" src="" /> */}
       </div>
       <div className="nav-button-container">
         <span className="support">
-          <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
-            <button className="support-btn">Track Status</button>
+          <a href="" className="supportbtn-link">
+            <button id="track-status">Track Status</button>
           </a>
         </span>
         <span className="signin">
           <a href="" className="signinbtn-link">
-            <button className="support-btn">Sign In</button>
+            <button id="sign-in">Sign In</button>
           </a>
         </span>
         <span className="support">
           <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
-            <button className="support-btn">Support</button>
+            <button id="support">Support</button>
           </a>
         </span>
       </div>
