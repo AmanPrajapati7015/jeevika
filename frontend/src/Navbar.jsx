b8e74e25f5476cc7c17386909b32dd04d931ee8c
Aman Prajapati <aman@Amans-MacBook-Air.local>
2024-04-12 16:09:02 +0530

    added navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,24 +1,34 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
-function Navbar(){
+import "./Navbar.css";
+import logo from "../assets/ghar.png";
+import Button from 'react-bootstrap/Button'
 
-    const navigate = useNavigate();
-
-    function signOut(){
-        localStorage.removeItem("token");
-        setUser("");
-        navigate("/");
-    }
-
-    return(<>
-    <div className="navbar">
-        <nav>
-            hehe
-        </nav>
-    </div>        
- 
-    </>)
+export default function Navbar() {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <label htmlFor="logo-name-ji logo-name-vika">
+        <font id="logo-name-ji">जी</font><font id="logo-name-vika">vika</font>
+        </label>
+        {/* <img className="logo-img" src="" /> */}
+      </div>
+      <div className="nav-button-container">
+        <span className="support">
+          <a href="" className="supportbtn-link">
+            <button id="track-status">Track Status</button>
+          </a>
+        </span>
+        <span className="signin">
+          <a href="" className="signinbtn-link">
+            <button id="sign-in">Sign In</button>
+          </a>
+        </span>
+        <span className="support">
+          <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
+            <button id="support">Support</button>
+          </a>
+        </span>
+      </div>
+    </div>
+  );
 }
-
-export default Navbar;
