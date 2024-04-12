d4ae2bdd8bb1fe73550add316384727594f0443b
AAYUSH <aayushg205@gmail.com>
2024-04-12 16:18:55 +0530

    adding section B

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import SectionB from "./form-elements/SectionB"
 
 export default function App(){
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <SectionB/>
     </>
   )
 }
