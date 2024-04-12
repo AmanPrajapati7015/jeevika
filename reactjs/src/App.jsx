2c234879cd5a2286e3e97d835529b6354857e4fd
bqka <aditya2005mendiratta@gmail.com>
2024-04-12 16:18:27 +0530

    SectionA, SectionC

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import SectionA from "./form-elements/SectionA";
+import SectionC from "./form-elements/SectionC";
 
 export default function App(){
   return (
     <>
     <Navbar />
+    <SectionA />
+    <SectionC />
     </>
   )
 }
