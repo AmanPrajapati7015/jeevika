c2c1a858a97ec43b7282eedcd816feb9f23000f9
AAYUSH <aayushg205@gmail.com>
2024-04-12 12:57:06 +0530

    adding navbar

--- reactjs/src/components/Navbar.jsx
+++ reactjs/src/components/Navbar.jsx
@@ -1,5 +1,29 @@
-export default function Navbar(){
+import './Navbar.css'
+import logo from '../assets/ghar.png'
+
+export default function Navbar() {
     return (
-        <div>Hi</div>
-    )
+      <div className="nav-container">
+        <span className="logo">
+          <img className="logo-img" src="" />
+        </span>
+        <div className="nav-button-container">
+            <span className="support">
+            <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
+              <button className="support-btn">Track Status</button>
+            </a>
+          </span>
+          <span className="signin">
+            <a href="" className="signinbtn-link">
+              <button className="support-btn">Sign In</button>
+            </a>
+          </span>
+          <span className="support">
+            <a href="https://vihaan.ieeedtu.in/" className="supportbtn-link">
+              <button className="support-btn">Support</button>
+            </a>
+          </span>
+        </div>
+      </div>
+    );
 }
